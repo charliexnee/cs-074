02c115e0c840a254aa702e92b73304af2f0a74f5
charliexnee <sophie.a.debs@gmail.com>
2019-10-31 22:24:26 -0400

    init assignment 3

--- assignment-3.py
+++ assignment-3.py
@@ -14,3 +14,18 @@ def sigmoid(weights,features):
     return value
 
 # Part B
+def log_likelihood(weights,features,actual_ys):
+    return log_likelihood
+# Part
+
+def learn_weights(actual_ys, features, num_iterations, learning_rate):
+    #initialize weights
+    weights = list();
+    weights.append(1)
+    for i in range(len(features)):
+        weights.append(1)
+    # for i in range(num_iterations):
+        # calculate gradientsfor all weights
+        # update weights (note, each weight can be updated likethis:weight[j]+=learning_rate * gradient[j])if i%1000==0:
+        # print(log_likelihood(weights,features,actual_ys))
+return weights
